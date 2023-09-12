cdabe66cd5aee4fb05c8bb6d534bd9618cf8e5d1
Screk <hoorex_@hotmail.com>
2023-09-12 22:10:56 +0200

    fix problem with images routes

--- src/components/home/Carrousel.jsx
+++ src/components/home/Carrousel.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './Carrousel.css';
 
 const Carousel = () => {
-  const images = ["../public/espana.png", "../public/reino-unido.png", "../public/francia.png", "../public/alemania.png", "../public/brasil.png"];
+  const images = ["/src/images/espana.png", "/src/images/reino-unido.png", "/src/images/francia.png", "/src/images/alemania.png", "/src/images/brasil.png"];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const prevImage = () => {
--- src/components/home/PoliticaGlobal.jsx
+++ src/components/home/PoliticaGlobal.jsx
@@ -7,14 +7,14 @@ const PoliticaGlobal = () => {
     <div className='PoliticaGlobal-container'>
         <h2>Cuestiones De Politica Global</h2>
         <ul>
-            <li><img src="/public/IA.png" alt="" /><h3>IA</h3></li>
-            <li><img src="/public/Privacidad.png" alt="" /><h3>Privacidad</h3></li>
-            <li><img src="/public/ciberseguridad.png" alt="" /><h3>Ciberseguridad</h3></li>
-            <li><img src="/public/PI.png" alt="" /><h3>PI</h3></li>
-            <li><img src="/public/comercio.png" alt="" /><h3>Comercio</h3></li>
-            <li><img src="/public/Transferencia.png" alt="" /><h3>Tranferencia de Datos Transfronterizos</h3></li>
-            <li><img src="/public/fuerza_laboral.png" alt="" /><h3>Fuerza Laboral</h3></li>
-            <li><img src="/public/obtencion.png" alt="" /><h3>Obtencion</h3></li>
+            <li><img src="/src/images/IA.png" alt="" /><h3>IA</h3></li>
+            <li><img src="/src/images/Privacidad.png" alt="" /><h3>Privacidad</h3></li>
+            <li><img src="/src/images/ciberseguridad.png" alt="" /><h3>Ciberseguridad</h3></li>
+            <li><img src="/src/images/PI.png" alt="" /><h3>PI</h3></li>
+            <li><img src="/src/images/comercio.png" alt="" /><h3>Comercio</h3></li>
+            <li><img src="/src/images/Transferencia.png" alt="" /><h3>Tranferencia de Datos Transfronterizos</h3></li>
+            <li><img src="/src/images/fuerza_laboral.png" alt="" /><h3>Fuerza Laboral</h3></li>
+            <li><img src="/src/images/obtencion.png" alt="" /><h3>Obtencion</h3></li>
         </ul>
     </div>
     </>
