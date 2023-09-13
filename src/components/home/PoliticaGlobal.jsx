4e10e82aa744eaed321c721aa796105d64ea8369
Screk <hoorex_@hotmail.com>
2023-09-13 15:44:33 +0200

    another try

--- src/components/home/PoliticaGlobal.jsx
+++ src/components/home/PoliticaGlobal.jsx
@@ -7,14 +7,14 @@ const PoliticaGlobal = () => {
     <div className='PoliticaGlobal-container'>
         <h2>Cuestiones De Politica Global</h2>
         <ul>
-            <li><img src="/public/images/IA.png" alt="" /><h3>IA</h3></li>
-            <li><img src="/public/images/Privacidad.png" alt="" /><h3>Privacidad</h3></li>
-            <li><img src="/public/images/ciberseguridad.png" alt="" /><h3>Ciberseguridad</h3></li>
-            <li><img src="/public/images/PI.png" alt="" /><h3>PI</h3></li>
-            <li><img src="/public/images/comercio.png" alt="" /><h3>Comercio</h3></li>
-            <li><img src="/public/images/Transferencia.png" alt="" /><h3>Tranferencia de Datos Transfronterizos</h3></li>
-            <li><img src="/public/images/fuerza_laboral.png" alt="" /><h3>Fuerza Laboral</h3></li>
-            <li><img src="/public/images/obtencion.png" alt="" /><h3>Obtencion</h3></li>
+            <li><img src="/public/images/IA.png" alt="texto" /><h3>IA</h3></li>
+            <li><img src="/public/images/Privacidad.png" alt="texto" /><h3>Privacidad</h3></li>
+            <li><img src="/public/images/ciberseguridad.png" alt="texto" /><h3>Ciberseguridad</h3></li>
+            <li><img src="/public/images/PI.png" alt="texto" /><h3>PI</h3></li>
+            <li><img src="/public/images/comercio.png" alt="texto" /><h3>Comercio</h3></li>
+            <li><img src="/public/images/Transferencia.png" alt="texto" /><h3>Tranferencia de Datos Transfronterizos</h3></li>
+            <li><img src="/public/images/fuerza_laboral.png" alt="texto" /><h3>Fuerza Laboral</h3></li>
+            <li><img src="/public/images/obtencion.png" alt="texto" /><h3>Obtencion</h3></li>
         </ul>
     </div>
     </>
