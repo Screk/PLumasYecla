ca35673440a7903818da7ee3fbb91151302c2e41
Screk <hoorex_@hotmail.com>
2023-09-14 13:21:19 +0200

    Another one2

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         </article>
         <article className='article2'>
           <nav>                                                   
-          <NavLink className='logo' to='/pages/home'><img src="/public/Caratula-web.jpg" alt="texto" /></NavLink>
+          <NavLink className='logo' to='/pages/home'><img src="public/Caratula-web.jpg" alt="texto" /></NavLink>
           </nav>
           <nav className='nav2'>
             <div className='link2_container'>
