4bb7f20c8735fad716d096600b312b9b9dd9987c
Screk <hoorex_@hotmail.com>
2023-09-14 13:27:48 +0200

    add final fixing routes

--- src/components/home/PoliticaGlobal.jsx
+++ src/components/home/PoliticaGlobal.jsx
@@ -7,14 +7,14 @@ const PoliticaGlobal = () => {
     <div className='PoliticaGlobal-container'>
         <h2>Cuestiones De Politica Global</h2>
         <ul>
-            <li><img src="/public/IA.png" alt="texto" /><h3>IA</h3></li>
-            <li><img src="/public/Privacidad.png" alt="texto" /><h3>Privacidad</h3></li>
-            <li><img src="/public/ciberseguridad.png" alt="texto" /><h3>Ciberseguridad</h3></li>
-            <li><img src="/public/PI.png" alt="texto" /><h3>PI</h3></li>
-            <li><img src="/public/comercio.png" alt="texto" /><h3>Comercio</h3></li>
-            <li><img src="/public/Transferencia.png" alt="texto" /><h3>Tranferencia de Datos Transfronterizos</h3></li>
-            <li><img src="/public/fuerza_laboral.png" alt="texto" /><h3>Fuerza Laboral</h3></li>
-            <li><img src="/public/obtencion.png" alt="texto" /><h3>Obtencion</h3></li>
+            <li><img src="../IA.png" alt="texto" /><h3>IA</h3></li>
+            <li><img src="../Privacidad.png" alt="texto" /><h3>Privacidad</h3></li>
+            <li><img src="../ciberseguridad.png" alt="texto" /><h3>Ciberseguridad</h3></li>
+            <li><img src="../PI.png" alt="texto" /><h3>PI</h3></li>
+            <li><img src="../comercio.png" alt="texto" /><h3>Comercio</h3></li>
+            <li><img src="../Transferencia.png" alt="texto" /><h3>Tranferencia de Datos Transfronterizos</h3></li>
+            <li><img src="../fuerza_laboral.png" alt="texto" /><h3>Fuerza Laboral</h3></li>
+            <li><img src="../obtencion.png" alt="texto" /><h3>Obtencion</h3></li>
         </ul>
     </div>
     </>
