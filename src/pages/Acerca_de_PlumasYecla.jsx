75d5f974f02a84caa0c0dec47b5ffd4016e68760
Screk <hoorex_@hotmail.com>
2023-09-21 20:23:03 +0200

    añade retoques

--- src/pages/Acerca_de_PlumasYecla.jsx
+++ src/pages/Acerca_de_PlumasYecla.jsx
@@ -7,19 +7,22 @@ const Acerca_de_PlumasYecla = () => {
   <div className='encuentranos_container'>
     <h1>Puedes encontrarnos aqui!</h1>
     <div>
-      <img src="../Nave.jpg" alt="" />
-    </div >
-    <div className='ficha' >
       <div>
-        <h2><strong>Dirección:</strong></h2> 
-        <h2><strong>Como llegar:</strong></h2> 
+        <img src="../Nave.jpg" alt="" />
+      </div >
+      <div className='ficha' >
+        <div>
+          <h2><strong>Dirección:</strong></h2> 
+          <h2><strong>Como llegar:</strong></h2> 
+        </div>
+        <div>
+          <p>C. Reina Sofía, 33, 30510 Yecla, Murcia</p>
+          <NavLink className='navlink' to='https://www.google.com/maps/place/Plumas+Yecla+S.L./@38.6159071,-1.0602775,17.25z/data=!4m6!3m5!1s0xd63fd2deaaaaaab:0xd07669cae2729cbe!8m2!3d38.6160466!4d-1.0608653!16s%2Fg%2F1hc7c0y2f?authuser=0&entry=ttu'><img src="../Maps_2.png" alt="" /></NavLink>
+        </div>
+        
       </div>
-      <div>
-        <p>C. Reina Sofía, 33, 30510 Yecla, Murcia</p>
-        <NavLink className='navlink' to='https://www.google.com/maps/place/Plumas+Yecla+S.L./@38.6159071,-1.0602775,17.25z/data=!4m6!3m5!1s0xd63fd2deaaaaaab:0xd07669cae2729cbe!8m2!3d38.6160466!4d-1.0608653!16s%2Fg%2F1hc7c0y2f?authuser=0&entry=ttu'><img src="../Maps_2.png" alt="" /></NavLink>
-      </div>
-      
     </div>
+    
       
   </div>
   )
--- src/pages/Fundacion_PLumasYecla.jsx
+++ src/pages/Fundacion_PLumasYecla.jsx
@@ -16,20 +16,27 @@ const Fundacion_PLumasYecla = () => {
       <div className='seccion_de_llenado'>
         <h2>Seccion de llenado</h2>
         <div className='imagenes_llenado'>
-          <img src="../imagenes_llenado1.jpg" alt="" />
-          <img src="../imagenes_llenado2.jpg" alt="" />
-          <img src="../imagenes_llenado3.jpg" alt="" />
-          <img src="../carrousel3.jpg" alt="" />
-          
+          <div>
+            <img src="../imagenes_llenado1.jpg" alt="" />
+            <img src="../imagenes_llenado2.jpg" alt="" />
+          </div>
+          <div>
+            <img src="../imagenes_llenado3.jpg" alt="" />
+            <img src="../carrousel3.jpg" alt="" />
+          </div>
         </div>
       </div>
       <div className='seccion_de_corte_y_cosido'>
         <h2>Seccion de corte y cosido</h2>
         <div className='imagenes_corte'>
-          <img src="../imagenes_corte_y_cosido1.jpg" alt="" />
-          <img src="../imagenes_corte_y_cosido2.jpg" alt="" />
-          <img src="../imagenes_corte_y_cosido3.jpg" alt="" />
-          <img src="../imagenes_corte_y_cosido4.jpg" alt="" />
+          <div>
+            <img src="../imagenes_corte_y_cosido1.jpg" alt="" />
+            <img src="../imagenes_corte_y_cosido2.jpg" alt="" />
+          </div>
+          <div>
+            <img src="../imagenes_corte_y_cosido3.jpg" alt="" />
+            <img src="../imagenes_corte_y_cosido4.jpg" alt="" />
+          </div>
         </div>
       </div>
     </div>
